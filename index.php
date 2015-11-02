8d81942d251d9e3f661884250bfef3a2ae5762d5
Olivier Hamoud Le Guellec <contact@holivier.me>
2015-11-02 23:59:15 +0100

    Initial commit (late)
    Choice of variables

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Olivier Hamoud Le Guellec
+ * Date: 01/11/15
+ * Time: 16:23
+ */
+
+/**
+ * AGILE
+ * XP
+ * Pair Programming
+ * Block 1
+ */
+
+
+//initializing variables
+
+$gearsHandle = 0; // 0 : up, 1 : down
+
+$light['green'] = 0; // 0 : off, 1 : on
+$light['yellow'] = 0;
+$light['red'] = 0;
+
+$gears['front'] = 0; // 0 : locked up, 1 : maneuvering, 2 : locked down
+$gears['left'] = 0;
+$gears['right'] = 0;
+
+$doors['front'] = 0; // 0 : closed, 1 : maneuvering, 2 : open
+$doors['left'] = 0;
+$doors['right'] = 0;
+
+
+
+$emergencyHydraulicCircuit = 0; // 0 : off, 1 : activated and locked
+
+
+?>

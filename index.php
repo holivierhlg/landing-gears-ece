92ae3176d38a8ed5e560203bdf981930cebaa567
Olivier Hamoud Le Guellec <contact@holivier.me>
2015-11-03 00:15:34 +0100

    Added PHP Session Support
    Olivier

--- index.php
+++ index.php
@@ -15,24 +15,28 @@
 
 
 //initializing variables
+session_start();
+if($_SESSION['variables']['set'] != 1) {
 
-$gearsHandle = 0; // 0 : up, 1 : down
+    $variable['gearsHandle'] = 0; // 0 : up, 1 : down
 
-$light['green'] = 0; // 0 : off, 1 : on
-$light['yellow'] = 0;
-$light['red'] = 0;
+    $variable['light']['green'] = 0; // 0 : off, 1 : on
+    $variable['light']['yellow'] = 0;
+    $variable['light']['red'] = 0;
 
-$gears['front'] = 0; // 0 : locked up, 1 : maneuvering, 2 : locked down
-$gears['left'] = 0;
-$gears['right'] = 0;
+    $variable['gears']['front'] = 0; // 0 : locked up, 1 : maneuvering, 2 : locked down
+    $variable['gears']['left'] = 0;
+    $variable['gears']['right'] = 0;
 
-$doors['front'] = 0; // 0 : closed, 1 : maneuvering, 2 : open
-$doors['left'] = 0;
-$doors['right'] = 0;
-
-
-
-$emergencyHydraulicCircuit = 0; // 0 : off, 1 : activated and locked
+    $variable['doors']['front'] = 0; // 0 : closed, 1 : maneuvering, 2 : open
+    $variable['doors']['left'] = 0;
+    $variable['doors']['right'] = 0;
 
 
+    $variable['emergencyHydraulicCircuit'] = 0; // 0 : off, 1 : activated and locked
+
+    $variable['set'] = 1;
+
+    $_SESSION['variables'] = $variable;
+}
 ?>
